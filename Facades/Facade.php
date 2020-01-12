e53e69626229c364b0dc4d0a65f92ded3167a481
NetworkRanger <17346503142@163.com>
2020-01-12 19:18:37 +0800

    update

---
+++ Facades/Facade.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Iphpjs package.
+ *
+ * (c) NetworkRanger <admin@iphpjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Iphpjs\Support\Facades;
+
+abstract class Facade
+{
+    abstract protected static function getFacadeRoot();
+
+    /**
+     * Handle dynamic method calls into the model.
+     *
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        return $this->getFacadeRoot()->$method(...$parameters);
+    }
+
+    /**
+     * Handle dynamic static method calls into the method.
+     *
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        return (new static)->$method(...$parameters);
+    }
+}
